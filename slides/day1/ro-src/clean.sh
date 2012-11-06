4a9aa0497899e1dfa68b53f2a6e3a7b7191baf81
Tudor Berariu <tudor.berariu@gmail.com>
2012-11-06 17:17:51 +0200

    Viterbi, alea, alea.

---
+++ slides/day1/ro-src/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -fR *.aux *.bbl *.blg *.log *.nav *.snm *.toc _region_.tex *.out *.vrb *.dvi
+rm -fR *~
---
+++ slides/day1/ro-src/m-files/forward.m
@@ -0,0 +1,9 @@
+...
+Scale = zeros (1, T); % Scale is an 1 x T matrix
+Alpha = zeros (T, N); % Alpha is a T x N matrix
+Beta = ones (T, N); % Beta is a T x N matrix
+%% Forward variables
+% alpha_disc-start - Write code below
+
+% alpha_disc-end - Write code above
+...
