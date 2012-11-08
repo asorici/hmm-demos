1b37a2ea592e8c3988cf87c99e94b7a19d528f8a
Tudor Berariu <tudor.berariu@gmail.com>
2012-11-08 03:53:36 +0200

    Ghidul 50%

---
+++ guide/m-files/baum-welch.m
@@ -0,0 +1,4 @@
+%% Reestimate A, B
+% maximization_disc-start - Write code below
+
+% maximization_disc-end - Write code above
---
+++ guide/m-files/output.m
@@ -0,0 +1,4 @@
+Please choose a test to run!
+Type "list" to list all available tests.
+Type "quit" to exit this tester.
+Test: 
